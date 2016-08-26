f58f75f7dc22691cf377a50235d68f548e9b7803
Bbender87 <bbender87@gmail.com>
2016-08-26 14:40:55 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+
+<footer class="footer" role="contentinfo">
+
+	<small>&copy; Brent Bender</small>
+	<nav class="nav">
+		<a href="index.php">Home</a>
+		<a href="webdesign.php">Web Design</a>
+		<a href="leather.php">Leather Design</a>
+		<a href="photos.php">Photography</a>
+		<a href="projects.php">Other Projects</a>
+		</nav>
+</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title><?php print $title ?></title>
+    <meta name = "description" content = "<?php print $description ?>">
+    <meta name = "author" content = "Brent Bender">
+    <link rel="stylesheet" type="text/css" href="/csstest.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400">
+  </head>
+
+  <body>
+
+<!-- Header -->
+    <header class="primary-header" role="banner">
+
+      <h1 class="logo">
+        <a href="homeTest.html">Brent <br> Bender</a>
+      </h1>
+      <nav class="navbar" role="navigation">
+        <a href="index.php">Home</a>
+        <a href="webdesign.php">Web Design</a>
+        <a href="leather.php">Leather Design</a>
+        <a href="photos.php">Photography</a>
+        <a href="projects.php">Other Projects</a>
+      </nav>
+    </header>
