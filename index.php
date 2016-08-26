20207ef6216270eea0d61bfd7cccefc8d2eb4bf6
Bbender87 <bbender87@gmail.com>
2016-08-26 14:36:14 -0700

    Added responsive design using flex boxes and php header & footer

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<?php include("inc/header.php");
+$title = "Webpage of Brent Bender";
+$description = "California based designer | Web development, Photography, Leathercraft and other projects";
+?>
+
+    <!-- Hero -->
+
+    <section class="hero">
+
+      <h2>Dedicated to the Craft of Construction and Design</h2>
+
+      <p>Displaying my creations for your inspiration and enjoyment</p>
+
+    </section>
+
+    <!-- Teasers -->
+    <div class="grid">
+
+      <!--Web Design-->
+
+      <section class="col-1" aria-labelledby="Web Design">
+        <a href="webdesign.php">
+          <h5>Web Design</h5>
+          <h3>Learn Website Design</h3>
+        </a>
+        <p>Create your own webpage with HTML and CSS. These are powerful tools that you can easily learn on your own.</p>
+      </section>
+
+
+      <section class="col-2" aria-labelledby="Leather Work">
+        <a href="leather.php">
+          <h5>Leather Design</h5>
+          <h3>My Leathercraft Portfolio</h3>
+        </a>
+        <p>See my Creations including Wallets, Purses, and Bags, and more.</p>
+      </section>
+
+
+      <section class="col-3" aria-labelledby="Other Projects">
+        <a href="projects.php">
+          <h5>Other Projects</h5>
+          <h3>Ongoing and Past Creations</h3>
+        </a>
+        <p>Road bikes, Carpentry, an Electric Guitar, and a few others.</p>
+      </section>
+
+    </div>
+
+    <!-- Footer -->
+
+<?php include("inc/footer.php"); ?>
